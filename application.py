046112311a2b0fbfa65d290330eaa971ce22a685
Kevin Behrens <kevin@thekbb.net>
2024-04-25 13:43:45 -0500

    Modularize and error level up
    1. extract DDB table init into a seperate function
    2. extract timestamp conversion into a seperate function
    3. extract quote sumbission into a seperate function
    4. use flask's abort function to handle errors

--- application.py
+++ application.py
@@ -1,34 +1,41 @@
-# app.py
-
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
 import boto3
 import datetime
 
 application = Flask(__name__)
 
-dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
-table_name = 'bruce-quotes'
-table = dynamodb.Table(table_name)
-
 MAX_INPUT_LENGTH = 300
+
+def init_dynamodb_table():
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+    table_name = 'bruce-quotes'
+    return dynamodb.Table(table_name)
+
+table = init_dynamodb_table()
+
+def convert_timestamp(quote):
+    if 'timestamp' in quote:
+        quote['timestamp'] = datetime.datetime.strptime(quote['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+    return quote
+
+def add_quote_to_table(quote, table):
+    quote_id = int(datetime.datetime.now().timestamp())
+    timestamp = str(datetime.datetime.now())
+    table.put_item(Item={'quote_id': quote_id, 'quote': quote, 'timestamp': timestamp})
 
 @application.route('/')
 def index():
     response = table.scan()
     quotes = sorted(response.get('Items', []), key=lambda x: x.get('quote_id', 0), reverse=True)
-    for quote in quotes:
-        if 'timestamp' in quote:
-            quote['timestamp'] = datetime.datetime.strptime(quote['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+    quotes = [convert_timestamp(quote) for quote in quotes]
     return render_template('index.html', quotes=quotes)
 
 @application.route('/add_quote', methods=['POST'])
 def add_quote():
     quote = request.form['quote']
     if len(quote) > MAX_INPUT_LENGTH:
-        return f"Input length exceeds maximum limit of {MAX_INPUT_LENGTH} characters.", 400
-    quote_id = int(datetime.datetime.now().timestamp())
-    timestamp = str(datetime.datetime.now())
-    table.put_item(Item={'quote_id': quote_id, 'quote': quote, 'timestamp': timestamp})
+        abort(400, description=f"Input length exceeds maximum limit of {MAX_INPUT_LENGTH} characters.")
+    add_quote_to_table(quote, table)
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
