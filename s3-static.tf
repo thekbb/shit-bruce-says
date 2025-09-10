8cbc335de7f76ce5058b13b9ff0981c5150e696d
Kevin Behrens <kevin@thekbb.net>
2025-09-10 14:29:35 -0500

    Add really long cache-control headers to static s3 assets

--- s3-static.tf
+++ s3-static.tf
@@ -20,49 +20,55 @@ resource "aws_s3_bucket_public_access_block" "site" {
 }
 
 resource "aws_s3_object" "css" {
-  bucket       = aws_s3_bucket.site.id
-  key          = "styles.css"
-  source       = "${path.module}/web/styles.css"
-  content_type = "text/css; charset=utf-8"
-  etag         = filemd5("${path.module}/web/styles.css")
+  bucket        = aws_s3_bucket.site.id
+  key           = "styles.css"
+  source        = "${path.module}/web/styles.css"
+  content_type  = "text/css; charset=utf-8"
+  cache_control = "public, max-age=31536000, immutable"
+  etag          = filemd5("${path.module}/web/styles.css")
 }
 
 resource "aws_s3_object" "favicon" {
-  bucket       = aws_s3_bucket.site.id
-  key          = "favicon.svg"
-  source       = "${path.module}/web/favicon.svg"
-  content_type = "image/svg+xml"
-  etag         = filemd5("${path.module}/web/favicon.svg")
+  bucket        = aws_s3_bucket.site.id
+  key           = "favicon.svg"
+  source        = "${path.module}/web/favicon.svg"
+  content_type  = "image/svg+xml"
+  cache_control = "public, max-age=31536000, immutable"
+  etag          = filemd5("${path.module}/web/favicon.svg")
 }
 
 resource "aws_s3_object" "index" {
-  bucket       = aws_s3_bucket.site.id
-  key          = "index.html"
-  content      = local.index_html
-  content_type = "text/html; charset=utf-8"
-  etag         = md5(local.index_html) # forces update when template changes
+  bucket        = aws_s3_bucket.site.id
+  key           = "index.html"
+  content       = local.index_html
+  content_type  = "text/html; charset=utf-8"
+  cache_control = "public, max-age=300"
+  etag          = md5(local.index_html) # forces update when template changes
 }
 
 resource "aws_s3_object" "js" {
-  bucket       = aws_s3_bucket.site.id
-  key          = "app.js"
-  source       = "${path.module}/web/app.js"
-  content_type = "application/javascript; charset=utf-8"
-  etag         = filemd5("${path.module}/web/app.js")
+  bucket        = aws_s3_bucket.site.id
+  key           = "app.js"
+  source        = "${path.module}/web/app.js"
+  content_type  = "application/javascript; charset=utf-8"
+  cache_control = "public, max-age=31536000, immutable"
+  etag          = filemd5("${path.module}/web/app.js")
 }
 
 resource "aws_s3_object" "robots" {
-  bucket       = aws_s3_bucket.site.id
-  key          = "robots.txt"
-  source       = "${path.module}/web/robots.txt"
-  content_type = "text/plain; charset=utf-8"
-  etag         = filemd5("${path.module}/web/robots.txt")
+  bucket        = aws_s3_bucket.site.id
+  key           = "robots.txt"
+  source        = "${path.module}/web/robots.txt"
+  content_type  = "text/plain; charset=utf-8"
+  cache_control = "public, max-age=86400"
+  etag          = filemd5("${path.module}/web/robots.txt")
 }
 
 resource "aws_s3_object" "sitemap" {
-  bucket       = aws_s3_bucket.site.id
-  key          = "sitemap.xml"
-  source       = "${path.module}/web/sitemap.xml"
-  content_type = "application/xml; charset=utf-8"
-  etag         = filemd5("${path.module}/web/sitemap.xml")
+  bucket        = aws_s3_bucket.site.id
+  key           = "sitemap.xml"
+  source        = "${path.module}/web/sitemap.xml"
+  content_type  = "application/xml; charset=utf-8"
+  cache_control = "public, max-age=86400"
+  etag          = filemd5("${path.module}/web/sitemap.xml")
 }
