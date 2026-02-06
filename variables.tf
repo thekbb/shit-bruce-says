cc50c13fb26f0c9b04827d229c3a2269fa41ed7e
Kevin Behrens <kevin@thekbb.net>
2026-02-06 11:54:56 -0600

    alpha sort variables

--- variables.tf
+++ variables.tf
@@ -27,12 +27,6 @@ variable "github_repo" {
   default     = "thekbb/shit-bruce-says"
 }
 
-variable "table_name" {
-  description = "Name of the DynamoDB table"
-  type        = string
-  default     = "bruce-quotes"
-}
-
 variable "lambda_api_s3_key" {
   description = "S3 key for the API Lambda deployment package"
   type        = string
@@ -43,4 +37,10 @@ variable "lambda_page_generator_s3_key" {
   description = "S3 key for the page generator Lambda deployment package"
   type        = string
   default     = "lambda/page-generator.zip"
+}
+
+variable "table_name" {
+  description = "Name of the DynamoDB table"
+  type        = string
+  default     = "bruce-quotes"
 }
