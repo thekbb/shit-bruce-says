125622fd87940ca4fb90b39ce2346b45f4eb451d
Kevin Behrens <kevin@thekbb.net>
2026-05-05 19:56:49 -0500

    Stop Terraform managing now-static homepage

--- locals.tf
+++ locals.tf
@@ -1,8 +1,3 @@
 locals {
-  # Render web/index.html.tpl with the API custom domain base URL
-  index_html = templatefile("${path.module}/web/index.html.tpl", {
-    api_base_url = "https://${aws_apigatewayv2_domain_name.api.domain_name}"
-  })
-
   name = "bruce-quotes"
 }
